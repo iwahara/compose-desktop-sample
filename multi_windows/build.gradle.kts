3f374f62fbcb15b1b729963a1a391e81545951ce
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-11-07 00:58:52 +0900

    複数ウィンドウのサンプルを追加

---
+++ multi_windows/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+    id("org.jetbrains.compose") version "0.1.0-m1-build62"
+}
+
+repositories {
+    jcenter()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+        }
+    }
+}
