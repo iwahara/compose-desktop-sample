3e0ddcc1f61c242d61bec6b6cf77d9b8e6bfcda3
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-11-07 00:37:57 +0900

    import

--- desktop_template/src/main/kotlin/main.kt
+++ desktop_template/src/main/kotlin/main.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.mutableStateOf
