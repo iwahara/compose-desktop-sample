390ecbcd8f7f400a504f633f074c4d9edefa249a
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-11-13 22:45:51 +0900

    FABまで実装

--- all_widgets/src/main/kotlin/main.kt
+++ all_widgets/src/main/kotlin/main.kt
@@ -1,11 +1,11 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -13,22 +13,38 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
-fun main() = Window(title = "Compose for Desktop", size = IntSize(300, 300)) {
-    var count = remember { mutableStateOf(0) }
+fun main() = Window(title = "Compose for Desktop", size = IntSize(600, 600)) {
+    val count = remember { mutableStateOf(0) }
+    val checked = remember { mutableStateOf(true) }
+    val fabCount = remember { mutableStateOf(0) }
     MaterialTheme {
-        Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-            Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                   onClick = {
-                       count.value++
-                   }) {
-                BasicText(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
-            }
-            Button(modifier = Modifier.align(Alignment.CenterHorizontally),
-                   onClick = {
-                       count.value = 0
-                    }) {
-                BasicText("Reset")
+        Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(
+                            onClick = {
+                                fabCount.value++
+                            }) {
+                        Icon(asset = Icons.Filled.Add)
+                    }
+                },
+        ) {
+            Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+                BasicText("Clicked ${count.value}!")
+                Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+                        onClick = {
+                            count.value++
+                        }) {
+                    BasicText("ボタンを押すよ！")
+                }
+                Checkbox(checked = checked.value, modifier = Modifier.align(Alignment.CenterHorizontally),
+                        onCheckedChange = {
+                            checked.value = it
+                        })
+                BasicText(if (checked.value) "チェック済み！" else "チェックなし！")
+                BasicText("Clicked ${fabCount.value}!")
             }
         }
+
+
     }
 }
