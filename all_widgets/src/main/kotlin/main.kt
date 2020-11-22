01be30eaae1b68fe2b5a885e323fe3f4c0c51417
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-11-22 23:47:06 +0900

    Scaffold内を整理

--- all_widgets/src/main/kotlin/main.kt
+++ all_widgets/src/main/kotlin/main.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,26 +30,13 @@ fun main() = Window(title = "Compose for Desktop", size = IntSize(600, 600)) {
         Scaffold(
                 scaffoldState = scaffoldState,
                 topBar = {
-                    TopAppBar(
-                            title = { Text("タイトル") },
-                            navigationIcon = {
-                                Icon(imageVector = Icons.Filled.Menu, modifier = Modifier.clickable(onClick =
-                                {
-                                    scaffoldState.drawerState.open() // or toggle
-                                }))
-                            },
-                    )
+                    topAppBar(scaffoldState)
                 },
                 floatingActionButton = {
-                    FloatingActionButton(
-                            onClick = {
-                                fabCount.value++
-                            }) {
-                        Icon(imageVector = Icons.Filled.Add)
-                    }
+                    floatingActionButton(fabCount)
                 },
                 floatingActionButtonPosition = FabPosition.End,
-                drawerContent = { Text(text = "drawerContent") },
+                drawerContent = { drawerContent() },
         ) {
             Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
                 Row {
@@ -126,6 +114,34 @@ fun main() = Window(title = "Compose for Desktop", size = IntSize(600, 600)) {
 
     }
 
+}
+
+@Composable
+fun drawerContent() {
+    Text(text = "drawerContent")
+}
+
+@Composable
+fun floatingActionButton(fabCount: MutableState<Int>) {
+    FloatingActionButton(
+            onClick = {
+                fabCount.value++
+            }) {
+        Icon(imageVector = Icons.Filled.Add)
+    }
+}
+
+@Composable
+fun topAppBar(scaffoldState: ScaffoldState) {
+    TopAppBar(
+            title = { Text("タイトル") },
+            navigationIcon = {
+                Icon(imageVector = Icons.Filled.Menu, modifier = Modifier.clickable(onClick =
+                {
+                    scaffoldState.drawerState.open() // or toggle
+                }))
+            },
+    )
 }
 
 @Composable
